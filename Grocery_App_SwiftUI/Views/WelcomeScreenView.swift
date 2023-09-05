5c8e2b35945b7e617db2a4451362e8ac961818f7
Kapil-digitalflake <kapil@digitalflake.com>
2023-09-05 07:41:39 +0530

    welcome screen file created

---
+++ Grocery_App_SwiftUI/Views/WelcomeScreenView.swift
@@ -0,0 +1,20 @@
+//
+//  WelcomeScreenView.swift
+//  Grocery_App_SwiftUI
+//
+//  Created by DigitalFlake Kapil Dongre on 05/09/23.
+//
+
+import SwiftUI
+
+struct WelcomeScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WelcomeScreenView_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeScreenView()
+    }
+}
