40defb49adfed8c812616381cb432730513b9dc2
Kapil-digitalflake <kapil@digitalflake.com>
2023-09-16 20:46:51 +0530

    password screen added

--- Grocery_App_SwiftUI/Views/UserAuthenticationScreen/ForgotPasswordScreenView.swift
+++ Grocery_App_SwiftUI/Views/UserAuthenticationScreen/ForgotPasswordScreenView.swift
@@ -8,8 +8,63 @@
 import SwiftUI
 
 struct ForgotPasswordScreenView: View {
+    var deviceWidth: CGFloat {
+        UIScreen.main.bounds.width
+    }
+    
+    var deviceHeight: CGFloat {
+        UIScreen.main.bounds.height
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            VStack(alignment: .center, spacing: 30){
+                
+                 Text("Forgot Password")
+                    .font(.title)
+                    .fontWeight(.semibold)
+                
+                Text("Lorem ipsum dolor sit amet, conseteur sadisciping elitr, sed diam nonumy")
+                    .foregroundColor(Color(hue: 1.0, saturation: 0.054, brightness: 0.35))
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 15.0)
+                
+                VStack(alignment: .center, spacing: 15){
+                HStack {
+                    Image("email")
+                        .resizable()
+                        .frame(width: 25, height: 20)
+                    TextField("", text: .constant("Email Address")).foregroundColor(Color.gray)
+                        .font(.system(size: 16))
+                }
+                .padding()
+                .background(.white)
+                .cornerRadius(10)
+                .frame(width: deviceWidth - 25)
+                
+                Button{
+                    print("send link btn pressed")
+                } label: {
+                    Text("Send Link")
+                        .foregroundColor(Color.white)
+
+                        .padding(.vertical, 20)
+                        .background(Color("primary"))
+                }
+                .frame(width: deviceWidth - 25)
+                .background(Color("primary"))
+                .cornerRadius(10)
+            }
+                
+                
+                
+            }
+            .frame(width: deviceWidth)
+        
+        .padding(.top, 50)
+            Spacer()
+        }
+        .background(Color(hue: 0.565, saturation: 0.06, brightness: 0.974))
     }
 }
 
